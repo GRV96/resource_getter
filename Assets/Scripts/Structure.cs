97f7e30db5e2d104a345d95d55b3f8b153b29a8a
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2023-09-30 15:59:59 -0400

    Structure created

--- Assets/Scripts/Structure.cs
+++ Assets/Scripts/Structure.cs
@@ -9,14 +9,34 @@ public class Structure: MonoBehaviour {
 	private static ResourceGetter _greenGetter = new ResourceGetter("Materials/Green");
 	private static ResourceGetter _redGetter = new ResourceGetter("Materials/Red");
 
+	private const string NAME_HORIZONTAL_BEAM = "HorizontalBeam";
+	private const string NAME_LEFT_POLE = "LeftPole";
+	private const string NAME_RIGHT_POLE = "RightPole";
+
 	private GameObject _horizontalBeam = null;
 	private GameObject _leftPole = null;
 	private GameObject _rightPole = null;
 
 	void Awake() {
+		GameObject polePrefab = _polePrefabGetter.Resource;
 		_horizontalBeam = Instantiate(_horBeamPrefabGetter.Resource, transform);
-		_leftPole = Instantiate(_polePrefabGetter.Resource, transform);
-		_rightPole = Instantiate(_polePrefabGetter.Resource, transform);
-		Debug.Log(": ");
+		_horizontalBeam.name = NAME_HORIZONTAL_BEAM;
+		_leftPole = Instantiate(polePrefab, transform);
+		_leftPole.name = NAME_LEFT_POLE;
+		_rightPole = Instantiate(polePrefab, transform);
+		_rightPole.name = NAME_RIGHT_POLE;
+
+		Transform beamTransform = _horizontalBeam.transform;
+		Transform rightPoleTransform = _rightPole.transform;
+		Vector3 beamScale = beamTransform.localScale;
+		float poleHeight = rightPoleTransform.localScale.y;
+
+		Vector3 beamPosition = new Vector3(0f, poleHeight + beamScale.y / 2, 0f);
+		beamTransform.position = beamPosition;
+
+		float polePositionX = (beamScale.x - 3) / 2;
+		float polePositionY = poleHeight / 2;
+		rightPoleTransform.position = new Vector3(polePositionX, polePositionY, 0f);
+		_leftPole.transform.position = new Vector3(-polePositionX, polePositionY, 0f);
 	}
 }
